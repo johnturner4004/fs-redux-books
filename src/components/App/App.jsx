b1bfe2d52983560dfe437e9738b6179254a8f88c
johnturner4004 <johnturner4004@gmail.com>
2021-05-05 14:40:10 -0400

    add axios.get to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,9 +3,27 @@ import BookForm from '../BookForm/BookForm';
 
 import './App.css';
 
+import{useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import axios from 'axios';
+
 function App() {
 
+  const dispatch = useDispatch();
+
   // TODO - GET Book List from server
+  useEffect(()=> {
+    axios.get('/books')
+    .then(response =>{
+      // Send to reducer
+      dispatch({type: `SET_BOOK_LIST`, payload: response.data});
+    })
+    .catch(error => {
+      alert(`Sorry. Things aren't working at the moment. Try again later`);
+      console.log(`Error getting books`, error);
+    })
+  })
+
 
   return (
     <div className="App">
